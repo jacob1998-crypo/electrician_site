e82fd27c86548d5c5a7ee92b7d389f2ddb939504
Jacob Kaburu <mutitujacob1998@gmail.com>
2025-09-02 14:05:27 +0300

    Updated settings, homepage, and navbar

--- electrician_site/settings.py
+++ electrician_site/settings.py
@@ -10,21 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from pathlib import Path
-import os 
+import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)l9iwq#l9wfd#d#9r&1ah*-^di=52_n6xy@$=icd(o38_)c5-'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 
+    'django-insecure-s)l9iwq#l9wfd#d#9r&1ah*-^di=52_n6xy@$=icd(o38_)c5-'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.42.174', '*']
+# ALLOWED_HOSTS from environment variable, defaults to '*'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
